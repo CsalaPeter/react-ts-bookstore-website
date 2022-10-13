4e550b6e40977eb7258c5b7d28c57cc4cc168f85
CsalaPeter <csalapeter95@gmail.com>
2022-10-13 15:14:44 +0200

    Base setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import "./styles/App.css";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <p>Hello</p>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,10 @@
+type StoreItemProps = {
+  id: number;
+  name: string;
+  author: string;
+  genre: string;
+  publication_year: number;
+  price: number;
+};
+
+export function StoreItem() {}
